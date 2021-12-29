f9a983736533a0eff1228e099e57cdf570e1b096
omerozoglu <omerozoglu.1@gmail.com>
2021-12-29 13:32:42 +0300

    jquery

--- Clients/AngularClient/src/app/components/pages/vote/vote.component.ts
+++ Clients/AngularClient/src/app/components/pages/vote/vote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare let $: any;
 @Component({
   selector: 'app-vote',
   templateUrl: './vote.component.html',
@@ -11,5 +11,21 @@ export class VoteComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  ngAfterViewInit() {
+    $(document).ready(() => {
+      $('input[type="radio"]').on("change", function (e: any) {
+        var candidate = $(e.target).parents(".candidate")[0];
+        $(".candidate").removeClass("active");
+        $(candidate).addClass("active");
+      });
+      $(".circle").on("click", function (e: any) {
+        var radio = $(e.target).find('input[type="radio"]');
+        console.log();
 
+        radio.checked = true;
+        $(radio).trigger("change");
+      });
+    });
+
+  }
 }
