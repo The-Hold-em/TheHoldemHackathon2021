54874b306530780093d6550d61a011f14340c1ce
omerozoglu <omerozoglu.1@gmail.com>
2021-12-29 13:01:03 +0300

    Angular

--- Clients/AngularClient/src/app/app-routing.module.ts
+++ Clients/AngularClient/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { VoteComponent } from './components/pages/vote/vote.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent }
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'vote', component: VoteComponent }
 ];
 
 @NgModule({
